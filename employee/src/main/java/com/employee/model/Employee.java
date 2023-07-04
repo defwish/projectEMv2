44f19db4bba97b0305f85ddc05bd54041fa6eb9f
defwish <58536015+defwish@users.noreply.github.com>
2023-07-04 09:59:10 +0300

    Add files via upload

---
+++ employee/src/main/java/com/employee/model/Employee.java
@@ -0,0 +1,71 @@
+package com.employee.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String position;
+
+    // Constructors, getters, and setters
+    public Employee() {
+        // Default constructor
+    }
+
+    public Employee(String name, String email, String position) {
+        this.name = name;
+        this.email = email;
+        this.position = position;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", position='" + position + '\'' +
+                '}';
+    }
+
+}
